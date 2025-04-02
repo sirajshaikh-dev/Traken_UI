29b7d0b09c4977262185e830f65b7ee519ca7214
sirajshaikh-dev <ss1425306@gmail.com>
2025-04-02 17:50:57 +0530

    add calendar2, modify app.tsx

--- src/layout/docs/app.tsx
+++ src/layout/docs/app.tsx
@@ -1,16 +1,27 @@
-// import Accordion from "@/components/accordion/Accordion";
-// import AccordionItem from "@/components/accordion/AccordionItem";
-import { Calendar } from "@/components/Calendar";
-// import Calendar from "@/components/Calendar/Calendar";
+import { Calendar } from "@/components/Calendar2/Calendar";
 import { useState } from "react";
 
 const Example = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const [selectedDates, setSelectedDates] = useState<Date[]>([]);
+
+  const handleDateSelect = (date: Date) => {
+    setSelectedDates([date]);
+  };
+
   return (
     <div className="p-10">
-    <h1 className="text-2xl font-bold mb-4">Select a Date</h1>
-    <Calendar selectedDate={selectedDate} onSelectDate={setSelectedDate} />
-  </div>
+      <h1 className="text-2xl font-bold mb-4">Select a Date</h1>
+      <Calendar
+        selectedDates={selectedDates}
+        onDateSelect={handleDateSelect}
+        disablePast={false}
+        disableFuture={false}
+        variant="month-year-picker"
+        color="default"
+        size="md"
+      />
+    </div>
   );
 };
 
